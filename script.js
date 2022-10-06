52d003c5987db989bbd41e2d9fc8be7acbe14a9d
Yury Stolarz Santana <yurysscareer@gmail.com>
2022-10-06 16:05:36 -0300

    subindo projeto repositorio pessoal

--- script.js
+++ script.js
@@ -2,24 +2,32 @@ const bolas = document.getElementsByClassName('ball');
 const text = document.getElementById('answer');
 const corParaTentarAcertar = document.getElementById('rgb-color');
 const btn = document.getElementById('reset-game');
-const score = document.getElementById('score');
+const acertos = document.getElementById('acertou');
+const erros = document.getElementById('errou');
 function corParaAcertar() {
   let r = Math.floor(Math.random() * 256);
   let g = Math.floor(Math.random() * 256);
   let b = Math.floor(Math.random() * 256);
   corParaTentarAcertar.innerText = '(' + r + ', ' + g + ', ' + b + ')';
 }
-let control = 0;
+let win = 0;
+let lose = 0;
 function jogo(e) {
   let acertou = 'rgb' +  corParaTentarAcertar.innerText;
   const alvo = e;
   if (alvo.target.style.backgroundColor === acertou) {
-    text.innerText = 'Acertou!';
-    control += 3;
-    score.innerText = 'Placar: ' + control;
+    text.innerText = 'Acertou!!!!';
+    win += 1;
+    acertos.innerText = 'Acertos: ' + win;
   } else {
     text.innerText = 'Errou! Tente novamente!';
+    lose += 1;
+    erros.innerText = 'Erros: ' + lose;
   }
+}
+function resetColor() {
+  corParaAcertar();
+  gerarCorDasBolas();
 }
 function gerarCorDasBolas() {
   for (let i = 0; i < bolas.length; i += 1) {
@@ -27,6 +35,7 @@ function gerarCorDasBolas() {
     let g = Math.floor(Math.random() * 256);
     let b = Math.floor(Math.random() * 256);
     bolas[i].addEventListener('click', jogo);
+    bolas[i].addEventListener('click', resetColor);
     bolas[i].style.backgroundColor = 'rgb' + '(' + r + ', ' + g + ', ' + b + ')';
   }
   let corBolaCerta = Math.floor(Math.random() * 6);
@@ -36,6 +45,10 @@ function resetGame() {
   corParaAcertar();
   gerarCorDasBolas();
   text.innerText = 'Escolha uma cor';
+  win = 0;
+  lose = 0;
+  erros.innerText = 'Erros: 0';
+  acertos.innerText = 'Acertos : 0';
 }
 btn.addEventListener('click', resetGame);
 window.onload = function aleatoria() {
